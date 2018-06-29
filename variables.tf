b2df3766188f4c48cc0c5ca7cb5b375e175ff865
Alexander Mancevice <amancevice@cargometrics.com>
2018-06-28 20:40:08 -0400

    slackbot ~> 0.1.5

--- variables.tf
+++ variables.tf
@@ -13,7 +13,3 @@ variable "aws_region" {
 variable "slack_verification_token" {
   description = "Slack verification token."
 }
-
-variable "slack_web_api_token" {
-  description = "Slack Web API token."
-}

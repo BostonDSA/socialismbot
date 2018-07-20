c358ae44bc20d930f754f1320209a2463b85dd07
Alexander Mancevice <amancevice@cargometrics.com>
2018-07-19 21:02:36 -0400

    localize channels

--- main.tf
+++ main.tf
@@ -5,9 +5,15 @@ provider "aws" {
   version    = "~> 1.27"
 }
 
+locals {
+  events    = "C7F7Z0WJG"
+  team_mods = "G7FAX48KX"
+  testing   = "GB1SLKKL7"
+}
+
 module "socialismbot" {
   source                  = "amancevice/slackbot/aws"
-  version                 = "1.2.0"
+  version                 = "1.3.0"
   api_name                = "socialismbot"
   slack_user_access_token = "${var.slack_user_access_token}"
   slack_bot_access_token  = "${var.slack_bot_access_token}"
@@ -26,8 +32,7 @@ module "socialismbot_mod" {
   source             = "amancevice/slackbot-mod/aws"
   version            = "0.1.2"
   api_name           = "${module.socialismbot.api_name}"
-  moderation_channel = "G7FAX48KX"
-  # moderation_channel = "GB1SLKKL7"
+  moderation_channel = "${local.team_mods}"
   role_arn           = "${module.socialismbot.slackbot_role_arn}"
   secret             = "${module.socialismbot.secret}"
 }

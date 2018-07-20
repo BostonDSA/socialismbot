66c1b5522295d6beed95eaeb3079b9bdfa7b6b5f
Alexander Mancevice <amancevice@cargometrics.com>
2018-07-19 21:11:13 -0400

    sns-messenger ~> 3.1.1

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ module "socialismbot_mod" {
 
 module "socialismbot_sns_messenger" {
   source   = "amancevice/slackbot-sns-messenger/aws"
-  version  = "3.1.0"
+  version  = "3.1.1"
   api_name = "${module.socialismbot.api_name}"
   role_arn = "${module.socialismbot.slackbot_role_arn}"
   secret   = "${module.socialismbot.secret}"

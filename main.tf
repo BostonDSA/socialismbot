7d2ea35b9c576a06e34212e0248f92ad94f68d42
Alexander Mancevice <amancevice@cargometrics.com>
2018-07-22 12:34:21 -0400

    upgrade aws plugin

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   access_key = "${var.aws_access_key_id}"
   secret_key = "${var.aws_secret_access_key}"
   region     = "${var.aws_region}"
-  version    = "~> 1.27"
+  version    = "~> 1.28"
 }
 
 locals {

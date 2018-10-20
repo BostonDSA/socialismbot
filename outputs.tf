c09be0cc39d61c6d7c097820c0e693557e260e08
Alexander Mancevice <amancevice@cargometrics.com>
2018-10-20 14:19:36 -0400

    remove quotes

--- outputs.tf
+++ outputs.tf
@@ -1,59 +1,59 @@
-output "mod_topic_arns" {
+output mod_topic_arns {
   description = "Moderator SNS Topic ARNs."
   value       = ["${module.moderator.topic_arns}"]
 }
 
-output "socialismbot_api_execution_arn" {
+output socialismbot_api_execution_arn {
   description = "REST API deployment execution ARN."
   value       = "${module.socialismbot.api_execution_arn}"
 }
 
-output "socialismbot_api_invoke_url" {
+output socialismbot_api_invoke_url {
   description = "REST API deployment invocation URL."
   value       = "${module.socialismbot.api_invoke_url}"
 }
 
-output "socialismbot_api_name" {
+output socialismbot_api_name {
   description = "REST API Name."
   value       = "${module.socialismbot.api_name}"
 }
 
-output "socialismbot_api_proxy_resource_id" {
+output socialismbot_api_proxy_resource_id {
   description = "API Gateway proxy resource ID."
   value       = "${module.socialismbot.api_proxy_resource_id}"
 }
 
-output "socialismbot_kms_key_id" {
+output socialismbot_kms_key_id {
   description = "KMS Key ID."
   value       = "${module.socialismbot.kms_key_id}"
 }
 
-output "socialismbot_lambda_name" {
+output socialismbot_lambda_name {
   description = "API Lambda name."
   value       = "${module.socialismbot.lambda_name}"
 }
 
-output "socialismbot_request_urls" {
+output socialismbot_request_urls {
   description = "Callbacks Request URL."
   value        = "${module.socialismbot.request_urls}"
 }
 
-output "socialismbot_role_name" {
+output socialismbot_role_name {
   description = "Role for Slackbot lambdas."
   value       = "${module.socialismbot.role_name}"
 }
 
-output "socialismbot_secret_name" {
+output socialismbot_secret_name {
   description = "Slackbot SecretsManager secret name."
   value       = "${module.socialismbot.secret_name}"
 }
 
-output "socialismbot_secrets_policy_arn" {
+output socialismbot_secrets_policy_arn {
   description = "Slackbot KMS key decryption permission policy ARN."
   value       = "${module.socialismbot.secrets_policy_arn}"
 }
 
-output "messenger_topic_arn" {
+output messenger_topic_arn {
   description = "SNS Topic ARN for publishing messages to Slack."
   value       = "${module.messenger.topic_arn}"
 }
--- variables.tf
+++ variables.tf
@@ -1,31 +1,31 @@
-variable "aws_access_key_id" {
+variable aws_access_key_id {
   description = "AWS Access Key ID."
 }
 
-variable "aws_secret_access_key" {
+variable aws_secret_access_key {
   description = "AWS Secret Access Key."
 }
 
-variable "aws_region" {
+variable aws_region {
   description = "AWS Region."
 }
 
-variable "slack_bot_access_token" {
+variable slack_bot_access_token {
   description = "Slack OAuth bot access token."
 }
 
-variable "slack_client_id" {
+variable slack_client_id {
   description = "Slack Client ID."
 }
 
-variable "slack_client_secret" {
+variable slack_client_secret {
   description = "Slack Client Secret."
 }
 
-variable "slack_signing_secret" {
+variable slack_signing_secret {
   description = "Slack signing secret."
 }
 
-variable "slack_user_access_token" {
+variable slack_user_access_token {
   description = "Slack OAuth user access token."
 }
